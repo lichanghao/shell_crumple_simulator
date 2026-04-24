b560612a0ca6ddb29339c0ce2b7d764851bf6e6c
lichanghao <542103276@qq.com>
2026-04-24 15:17:29 -0400

    Restore cyclic free-minimize handoff parity

--- src/core/solver.cpp
+++ src/core/solver.cpp
@@ -728,7 +728,9 @@ MinimizeFreeResult minimize_free(const SimulatorInput& input,
         return {res.total_energy, extract_free_gradient(res, bcs)};
     };
 
-    int flag = solver.minimize(x_free, xnorm0, /*stop_on_first_trial=*/false, callback);
+    const bool stop_on_first_trial =
+        (bcs.nCodeLoad == 30 || bcs.nCodeLoad == 31);
+    int flag = solver.minimize(x_free, xnorm0, stop_on_first_trial, callback);
 
     // Broadcast and scatter final state.
     double gnorm = solver.gnorm();
